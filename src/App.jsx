1802ecd7a589d9c39fdfe61eedcc5e0eb0d94736
Vansh Gulati <vanshgulati@gmail.com>
2025-02-22 14:00:44 +0530

    URL Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import ShowUser from "./pages/ShowUser";
 
 const App = () => {
   return (
-    <Router>
+    // <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/show-user" element={<ShowUser />} />
       </Routes>
-    </Router>
+    // </Router>
   );
 };
 
